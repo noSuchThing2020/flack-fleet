1e6ab9b74004d05cab9f2f0b2520a23634815b9c
noSuchThing2020 <loverlee1990@gmail.com>
2020-05-13 23:33:39 +0800

    cors_allowed_origins=

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 print(os.getenv("SECRET_KEY"))
 
 channels = ["Lobby","Coding", "Stocks", "Sports", "Study"]
